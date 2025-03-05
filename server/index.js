2e6227a37d648258db55755522c4f0944c60349f
Atharv-28 <atharvmt@outlook.com>
2025-03-05 20:40:53 +0530

    backend up

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
 const port = 5000;
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+async function run() {
+
+app.get('/getUrlQR', (req, res) => {
+    const url = req.query.url;
+    const qr = url;
+    res.send(qr);
+});
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
