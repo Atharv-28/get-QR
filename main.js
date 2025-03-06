abd50a6665631a0d924d60b3db63c57827237d98
Atharv-28 <atharvmt@outlook.com>
2025-03-06 12:55:28 +0530

    #1 is fixed
    line 34

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let output = document.getElementById("output");
 output.style.visibility = "hidden";
 let submit = document.getElementById("generate");
 submit.addEventListener("click", function (e) {
+  e.preventDefault();
   let url = document.getElementById("inputUrl").value;
   if (url === "") {
     alert("Please enter a URL !");
@@ -24,11 +25,11 @@ submit.addEventListener("click", function (e) {
         // For example, display the QR code in the output element
         // output.innerHTML = `<img src="${data.qr}" alt="QR Code">`;
         output.style.visibility = "visible";
-        e.preventDefault();
 
       })
       .catch((error) => {
         console.error('Error:', error);
       });
   }
+  return false;
 });
