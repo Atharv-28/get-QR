f8568ec1042945e6ed91eb012a29760fd10ab4e0
Atharv-28 <atharvmt@outlook.com>
2025-03-06 11:49:10 +0530

    qr block hidden

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+let output = document.getElementById("output");
+output.style.visibility = "hidden";
 let submit = document.getElementById("generate");
 submit.addEventListener("click", function () {
   let url = document.getElementById("inputUrl").value;
-  console.log(url);
+  if (url === "") {
+    alert("Please enter a URL !");
+    return;
+  }
+  else{
+    console.log(url);
+    output.style.visibility = "visible";
+  }
 });
